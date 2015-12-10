a3fdaf3227ce8d4894f444a7bb4dde22197fe4b8
Adam J Shook <adamjshook@gmail.com>
2015-12-09 19:06:43 -0500

    De-couple accumulo row deserializer from Cursor class.  Create a basic value-as-string deserializer

--- src/main/java/bloomberg/presto/accumulo/AccumuloRecordCursor.java
+++ src/main/java/bloomberg/presto/accumulo/AccumuloRecordCursor.java
@@ -20,10 +20,8 @@ import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.accumulo.core.client.IteratorSetting;
@@ -35,11 +33,9 @@ import org.apache.hadoop.io.Text;
 
 import com.facebook.presto.spi.RecordCursor;
 import com.facebook.presto.spi.type.Type;
-import com.google.common.base.Strings;
 
 import io.airlift.log.Logger;
 import io.airlift.slice.Slice;
-import io.airlift.slice.Slices;
 
 public class AccumuloRecordCursor implements RecordCursor {
 
@@ -51,32 +47,34 @@ public class AccumuloRecordCursor implements RecordCursor {
     private final long totalBytes = fieldValue.getBytes().length;
     private Scanner scan = null;
     private Iterator<Entry<Key, Value>> iterator = null;
-    private Map<String, Object> columnValues = new HashMap<>();
-    private Text cf = new Text(), cq = new Text(), value = new Text();
-    private Map<String, Map<String, String>> f2q2pc = new HashMap<>();
     private long start, end;
+    private AccumuloRowDeserializer deserializer;
 
-    public AccumuloRecordCursor(List<AccumuloColumnHandle> columnHandles,
+    public AccumuloRecordCursor(List<AccumuloColumnHandle> cHandles,
             Scanner scan) {
-        this.columnHandles = columnHandles;
+        LOG.debug("Constructor");
+        this.columnHandles = cHandles;
         this.scan = scan;
+        deserializer = AccumuloRowDeserializer.getDefault();
 
         IteratorSetting cfg = new IteratorSetting(1, "whole-key-value-iterator",
                 WholeColumnFamilyIterator.class);
         this.scan.addScanIterator(cfg);
 
-        fieldToColumnName = new String[columnHandles.size()];
+        fieldToColumnName = new String[cHandles.size()];
         Text fam = new Text(), qual = new Text();
-        for (int i = 0; i < columnHandles.size(); i++) {
-            AccumuloColumnHandle columnHandle = columnHandles.get(i);
-            fieldToColumnName[i] = columnHandle.getColumnName();
+        for (int i = 0; i < cHandles.size(); i++) {
+            AccumuloColumnHandle cHandle = cHandles.get(i);
+            fieldToColumnName[i] = cHandle.getColumnName();
 
-            fam.set(columnHandle.getColumnFamily());
-            qual.set(columnHandle.getColumnQualifier());
+            deserializer.setMapping(cHandle.getColumnName(),
+                    cHandle.getColumnFamily(), cHandle.getColumnQualifier());
+
+            fam.set(cHandle.getColumnFamily());
+            qual.set(cHandle.getColumnQualifier());
             this.scan.fetchColumn(fam, qual);
-            Map<String, String> q2pc = new HashMap<>();
-            q2pc.put(qual.toString(), columnHandle.getColumnName());
-            f2q2pc.put(fam.toString(), q2pc);
+            LOG.debug(String.format("Column %s maps to Accumulo column %s:%s",
+                    cHandle.getColumnName(), fam, qual));
         }
 
         iterator = this.scan.iterator();
@@ -108,23 +106,7 @@ public class AccumuloRecordCursor implements RecordCursor {
     public boolean advanceNextPosition() {
         try {
             if (iterator.hasNext()) {
-                columnValues.clear();
-
-                Entry<Key, Value> row = iterator.next();
-                for (Entry<Key, Value> kvp : WholeColumnFamilyIterator
-                        .decodeColumnFamily(row.getKey(), row.getValue())
-                        .entrySet()) {
-                    kvp.getKey().getColumnFamily(cf);
-                    kvp.getKey().getColumnQualifier(cq);
-                    value.set(kvp.getValue().get());
-                    String prestoColumn = f2q2pc.get(cf.toString())
-                            .get(cq.toString());
-                    columnValues.put(prestoColumn, value.toString());
-                    LOG.debug(String.format(
-                            "Put cf:cq %s:%s with value %s into presto column %s",
-                            cf.toString(), cq.toString(), value.toString(),
-                            prestoColumn));
-                }
+                deserializer.deserialize(iterator.next());
                 return true;
             } else {
                 end = System.nanoTime();
@@ -135,43 +117,44 @@ public class AccumuloRecordCursor implements RecordCursor {
         }
     }
 
-    private Object getFieldValue(int field) {
-        return columnValues.get(fieldToColumnName[field]);
+    @Override
+    public boolean isNull(int field) {
+        checkArgument(field < columnHandles.size(), "Invalid field index");
+        return deserializer.isNull(fieldToColumnName[field]);
     }
 
     @Override
     public boolean getBoolean(int field) {
         checkFieldType(field, BOOLEAN);
-        return Boolean.parseBoolean(getFieldValue(field).toString());
-    }
-
-    @Override
-    public long getLong(int field) {
-        checkFieldType(field, BIGINT);
-        return Long.parseLong(getFieldValue(field).toString());
+        return deserializer.getBoolean(fieldToColumnName[field]);
     }
 
     @Override
     public double getDouble(int field) {
         checkFieldType(field, DOUBLE);
-        return Double.parseDouble(getFieldValue(field).toString());
+        return deserializer.getDouble(fieldToColumnName[field]);
+    }
+
+    @Override
+    public long getLong(int field) {
+        checkFieldType(field, BIGINT);
+        return deserializer.getLong(fieldToColumnName[field]);
+    }
+
+    @Override
+    public Object getObject(int field) {
+        return deserializer.getObject(fieldToColumnName[field]);
     }
 
     @Override
     public Slice getSlice(int field) {
         checkFieldType(field, VARCHAR);
-        return Slices.utf8Slice(getFieldValue(field).toString());
+        return deserializer.getSlice(fieldToColumnName[field]);
     }
 
     @Override
-    public Object getObject(int field) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean isNull(int field) {
-        checkArgument(field < columnHandles.size(), "Invalid field index");
-        return columnValues.get(fieldToColumnName[field]) == null;
+    public void close() {
+        scan.close();
     }
 
     private void checkFieldType(int field, Type expected) {
@@ -179,10 +162,5 @@ public class AccumuloRecordCursor implements RecordCursor {
         checkArgument(actual.equals(expected),
                 "Expected field %s to be type %s but is %s", field, expected,
                 actual);
-    }
-
-    @Override
-    public void close() {
-        scan.close();
     }
 }
---
+++ src/main/java/bloomberg/presto/accumulo/StringRowDeserializer.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package bloomberg.presto.accumulo;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.user.WholeColumnFamilyIterator;
+import org.apache.hadoop.io.Text;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.airlift.log.Logger;
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+
+public class StringRowDeserializer implements AccumuloRowDeserializer {
+
+    private static final Logger LOG = Logger.get(StringRowDeserializer.class);
+    private Map<String, Map<String, String>> f2q2pc = new HashMap<>();
+    private Map<String, Object> columnValues = new HashMap<>();
+    private Text cf = new Text(), cq = new Text(), value = new Text();
+
+    @Override
+    public void setMapping(String name, String fam, String qual) {
+        columnValues.put(name, null);
+        f2q2pc.put(fam, ImmutableMap.<String, String> builder().put(qual, name)
+                .build());
+    }
+
+    @Override
+    public void deserialize(Entry<Key, Value> row) throws IOException {
+        columnValues.clear();
+
+        for (Entry<Key, Value> kvp : WholeColumnFamilyIterator
+                .decodeColumnFamily(row.getKey(), row.getValue()).entrySet()) {
+            kvp.getKey().getColumnFamily(cf);
+            kvp.getKey().getColumnQualifier(cq);
+            value.set(kvp.getValue().get());
+            String prestoColumn = f2q2pc.get(cf.toString()).get(cq.toString());
+            columnValues.put(prestoColumn, value.toString());
+            LOG.debug(String.format(
+                    "Put cf:cq %s:%s with value %s into presto column %s",
+                    cf.toString(), cq.toString(), value.toString(),
+                    prestoColumn));
+        }
+    }
+
+    @Override
+    public boolean isNull(String name) {
+        return columnValues.get(name) == null;
+    }
+
+    @Override
+    public boolean getBoolean(String name) {
+        return Boolean.parseBoolean(getFieldValue(name));
+    }
+
+    @Override
+    public double getDouble(String name) {
+        return Double.parseDouble(getFieldValue(name));
+    }
+
+    @Override
+    public long getLong(String name) {
+        return Long.parseLong(getFieldValue(name));
+    }
+
+    @Override
+    public Object getObject(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Slice getSlice(String name) {
+        return Slices.utf8Slice(getFieldValue(name));
+    }
+
+    private String getFieldValue(String name) {
+        return columnValues.get(name).toString();
+    }
+}
