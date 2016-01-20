3411572573be68aae57e7a7a0f6bc977a35e3dc8
Adam J Shook <adamjshook@gmail.com>
2016-01-19 19:08:36 -0500

    Add utility func to convert columns to indexed column structure

--- presto-accumulo/src/main/java/bloomberg/presto/accumulo/index/Utils.java
+++ presto-accumulo/src/main/java/bloomberg/presto/accumulo/index/Utils.java
@@ -2,12 +2,18 @@ package bloomberg.presto.accumulo.index;
 
 import java.nio.ByteBuffer;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.accumulo.core.data.ColumnUpdate;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.commons.lang.ArrayUtils;
+
+import bloomberg.presto.accumulo.model.AccumuloColumnHandle;
 
 public class Utils {
     private static final byte[] EMPTY_BYTES = new byte[0];
@@ -46,5 +52,23 @@ public class Utils {
                 }
             }
         }
+    }
+
+    public static Map<ByteBuffer, Set<ByteBuffer>> getMapOfIndexedColumns(
+            List<AccumuloColumnHandle> columns) {
+        Map<ByteBuffer, Set<ByteBuffer>> indexColumns = new HashMap<>();
+
+        for (AccumuloColumnHandle col : columns.stream()
+                .filter(x -> x.isIndexed()).collect(Collectors.toList())) {
+            ByteBuffer cf = ByteBuffer.wrap(col.getColumnFamily().getBytes());
+            Set<ByteBuffer> qualifies = indexColumns.get(cf);
+            if (qualifies == null) {
+                qualifies = new HashSet<>();
+                indexColumns.put(cf, qualifies);
+            }
+            qualifies.add(ByteBuffer.wrap(col.getColumnQualifier().getBytes()));
+        }
+
+        return indexColumns;
     }
 }
